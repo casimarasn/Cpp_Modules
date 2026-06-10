f5047e09b7f8ad8dabb2f32eb4eab24fdeefbbb5
msedeno- <msedeno-@student.42malaga.com>
2026-06-10 19:59:31 +0200

    10 JUN 26 19:59

--- Cpp00/ex01/src/phonebook.cpp
+++ Cpp00/ex01/src/phonebook.cpp
@@ -9,7 +9,7 @@ PhoneBook::~PhoneBook()
 				<< std::endl;
 }
 
-void	PhoneBook::printTable(int index)const
+void	PhoneBook::printTable()const
 {
 	std::cout	<< "| "
 				<< std::setw(10) << "Index" << " | "
@@ -42,7 +42,51 @@ void	PhoneBook::printTable(int index)const
 		std::cout << " |";
 		std::cout << std::endl;
 	}
-				
-				
-				
+}
+
+void	PhoneBook::addContact()
+{
+	std::string	first;
+	std::string	last;
+	std::string	nick;
+	std::string	phone;
+	std::string	secret;
+
+	do {
+		std::cout	<< "Cuál es tu Nombre?" << std::endl;
+		std::getline(std::cin, first);
+		if (first.empty())
+			std::cout	<< "Oye, tengo que saber cómo llamarte" << std::endl;
+	}while (first.empty());
+
+	do {
+		std::cout	<< "Y tu Appelido?" << std::endl;
+		std::getline(std::cin, last);
+		if (last.empty())
+			std::cout	<< "esa casta siempre presente,venga! " << std::endl;
+	}while (last.empty());
+	do {
+		std::cout	<< "Por qué Mote te conocen?" << std::endl;
+		std::getline(std::cin, nick);
+		if (!nick.empty())
+			std::cout	<< "el mote es tu marca de identidad, dime!" << std::endl;
+	}while (nick.empty());
+	do {
+		std::cout	<< "dime tu telefono" << std::endl;
+		std::getline(std::cin, phone);
+		if (phone.empty())
+			std::cout	<< "venga, ahora un telefono válido" << std::endl;
+	}while (phone.empty());
+	do {
+		std::cout << "Shhh!Cuentame un secreto ozzcuro" << std::endl;
+		std::getline(std::cin, secret);
+		if (secret.empty())
+		std::cout	<< "Venga soy una tumba!ese chisme aqui!" << std::endl;
+	}while (secret.empty());
+
+	_contacts[i].setFirstName(first);
+	_contacts[i].setLastName(last);
+	_contacts[i].setNickName(nick);
+	_contacts[i].setPhoneNumber(phone);
+	_contacts[i].setDarkestSecret(secret);
 }
