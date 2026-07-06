9924bf863276ad39d56fdf0f738ff8e090f2b04d
msedeno- <msedeno-@student.42malaga.com>
2026-07-06 18:12:30 +0200

    06 JUL 26 18:12

--- Cpp02/ex00/src/Fixed.cpp
+++ Cpp02/ex00/src/Fixed.cpp
@@ -1,31 +1,44 @@
 # include "Fixed.hpp"
 
-_fractionalBits{}
+const int Fixed::_fractionalBits = 8;
 
-Fixed::Fixed() : _fixedValue(0){}
-
-Fixed::~Fixed()
+Fixed::Fixed() : _fixedValue(0)
 {
-	std::cout	<< "Everything deleted"
+	std::cout	<< "Default constructor called"
 				<< std::endl;
 }
 
-Fixed::Fixed(const Fixed &original)
+Fixed::~Fixed()
 {
-
+	std::cout	<< "Destructor called"
+				<< std::endl;
 }
 
-Fixed Fixed::&operator=(const Fixed &other)
-{
+Fixed::Fixed(const Fixed &original):_fixedValue(original.getRawBits()){}
 
+Fixed&	Fixed::operator=(const Fixed &other)
+{
+	if (&other != this)
+		_fixedValue = other.getRawBits();
+	return(*this);
 }
 
 int Fixed::getRawBits( void ) const
-{
 
+{
+	std::cout	<< "getRawBits member function called"
+				<< std::endl;
+	return(_fixedValue);
 }
 
 void Fixed::setRawBits( int const raw)
 {
-
+	_fixedValue = raw;
 }
+
+
+Fixed a; dc 0
+Fixed b(a); cc a= 0
+Fixed c; dc 0, 
+
+c = a; -> op c= a= 0
