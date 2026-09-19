53e8a36182892f318b8f9aa532ca78fed84f0f88
msedeno- <msedeno-@student.42malaga.com>
2026-09-19 16:17:25 +0200

    19 SEP 26 16:17

--- Cpp06/ex00/src/ScalarConverter.cpp
+++ Cpp06/ex00/src/ScalarConverter.cpp
@@ -1,8 +1,46 @@
 # include "ScalarConverter.hpp"
 
+ScalarConverter::ScalarConverter(){}
 
-void	convert(std::string& lit)
+ScalarConverter::ScalarConverter(const ScalarConverter& src)
 {
-	if (lit == "-inff" || lit == "+inff" ||
-		lit == " nanf")
+	(void)src;
+}
+
+ScalarConverter	&ScalarConverter::operator=(const ScalarConverter& src)
+{
+	(void)src;
+	return(*this);
+}
+
+ScalarConverter::~ScalarConverter(){}
+
+void	ScalarConverter::convert(std::string& lit)
+{
+	bool	isPseudoLiteral = false;
+
+	if (lit == "-inff" || lit == "+inff" || lit == "nanf")
+	{
+		isPseudoLiteral = true;
+		// tipoDetectado = FLOAT;
+		std::cout << "He detectado un pseudo-literal float!" << std::endl;
+	}
+	else if (lit == "-inf" || lit == "+inf" || lit == "nan") 
+	{
+		isPseudoLiteral = true;
+		// tipoDetectado = DOUBLE;
+		std::cout << "He detectado un pseudo-literal double!" << std::endl;
+	}
+	else if (lit.length() == 1 && !std::isdigit(lit[0]))
+		// tipoDetectado = CHAR;
+		std::cout << "He detectado un char!" << std::endl;
+	else
+	{
+		for (int i = 0; i < lit.length(); i++)
+		{
+			/*saber si es int, char, float o double*/
+			if (lit == )
+		}
+
+	}
 }
